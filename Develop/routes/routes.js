1f180d60ec3efa48c691f4d3834f7c51fd455512
uchrissd <uchrissd@protonmail.com>
2020-02-03 20:10:43 -0800

    added new route

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -24,6 +24,17 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
 //put
 // router.post("/api/transaction/bulk", ({ body }, res) => {
 //   Transaction.insertMany(body)
